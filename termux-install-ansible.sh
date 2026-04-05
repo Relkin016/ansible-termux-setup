c6a6dbb8ceb0d9d86f5573c3b12708f18cfc1ead
Relkin016 <45715738+Relkin016@users.noreply.github.com>
2026-04-05 09:30:33 +0000

    Update termux-install-ansible.sh

--- termux-install-ansible.sh
+++ termux-install-ansible.sh
@@ -2,14 +2,22 @@
 yes | pkg upgrade  && \
 yes | pkg install \
   python \
-  python-dev \
+  python-pip \
   libffi \
-  libffi-dev \
   openssl \
-  openssl-dev \
   libsodium  \
   clang \
-  cmake
+  cmake \ 
+# Add new dependencies 
+  binutils
+  rust
+  build-essential
+
+# Add flags
+export CARGO_BUILD_TARGET=aarch64-linux-android
+export RUSTFLAGS="-C lto=n"
+# Setup Android API 
+export ANDROID_API_LEVEL=29 # !!Write your API LEVEL"
 # Install the latest Python package manager.
 # The version of pip that comes with Python may be outdated.
 pip install --upgrade pip 
@@ -17,7 +25,7 @@ pip list --outdated --format=freeze | \
   grep -v '^\-e' | \
   cut -d = -f 1 | \
   xargs -n1 pip install -U && \
-# The pynacl dependency originally did not install because
-# it gave problems building dependencies'
-pip install --upgrade pynacl 
-pip install --upgrade ansible
+# Upgrade depends 
+pip install --upgrade pip setuptools wheel
+pip install --upgrade pynacl  
+pip install --upgrade cryptography ansible pywinrm[credssp]
